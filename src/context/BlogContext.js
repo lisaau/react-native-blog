9965f32e68a796e765c246553c869eb711e33946
lisaau <lisaau@users.noreply.github.com>
2020-04-12 00:17:06 -0700

    Add delete post function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,17 +3,31 @@ import createDataContext from './createDataContext';
 // create reducer
 const blogReducer = (state, action) => {
     switch(action.type) {
+        case 'delete_blogpost':
+            return state.filter(blogpost => blogpost.id !== action.payload);
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }];
+            return [
+                ...state, 
+                { 
+                    id: Math.floor(Math.random() * 99999), 
+                    title: `Blog Post #${state.length + 1}` 
+                }
+            ];
         default:
             return state;
     }
 };
 
 // function that will dispatch an action that's going to modify the reducer
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
     return () => {
         dispatch({ type: 'add_blogpost' });
+    };
+};
+
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
     };
 }
 
@@ -21,6 +35,6 @@ const addBlogPost = (dispatch) => {
 // get Context and Provider that will make dat available to other parts of app
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 );
