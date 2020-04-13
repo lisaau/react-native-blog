bcd508cbf49102f80c3d760aab8d9b4da63aff15
lisaau <lisaau@users.noreply.github.com>
2020-04-12 22:09:39 -0700

    Initialize State from Context for EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Context } from '../context/BlogContext';
-import { EvilIcons } from '@expo/vector-icons'
+import { TextInput } from "react-native-gesture-handler";
+
 
 const EditScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+
+    const blogPost = state.find( blogPost => blogPost.id === navigation.getParam('id'));
+
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
+
     return (
         <View>
-            <Text>Edit Screen {navigation.getParam('id')}</Text>
+            <Text>Edit Title:</Text>
+            <TextInput value={title} onChangeText={ newTitle => setTitle(newTitle) } />
         </View>
     );
 };
